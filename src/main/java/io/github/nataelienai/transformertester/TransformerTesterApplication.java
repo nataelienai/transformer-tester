9a7f9ca1464eed2effd1e48454adc97c8fa2ac5f
Natã Elienai <nata.elienai@gmail.com>
2022-11-24 21:04:37 -0300

    feat: initial commit

---
+++ src/main/java/io/github/nataelienai/transformertester/TransformerTesterApplication.java
@@ -0,0 +1,13 @@
+package io.github.nataelienai.transformertester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformerTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformerTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nataelienai/transformertester/TransformerTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nataelienai.transformertester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformerTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
