f6bbe92f97f125a81c2d8817c80136a3b65d269b
Natã Elienai <nata.elienai@gmail.com>
2022-11-25 16:59:37 -0300

    feat: add User entity repository

---
+++ src/main/java/io/github/nataelienai/transformertester/user/UserRepository.java
@@ -0,0 +1,8 @@
+package io.github.nataelienai.transformertester.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
