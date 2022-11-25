f33c232bbf2563cd472b391b0f45f2c88a0c571e
Natã Elienai <nata.elienai@gmail.com>
2022-11-25 17:50:29 -0300

    feat: add UserInputDto class

---
+++ src/main/java/io/github/nataelienai/transformertester/user/UserInputDto.java
@@ -0,0 +1,15 @@
+package io.github.nataelienai.transformertester.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInputDto {
+
+  private String name;
+  private String email;
+
+}
