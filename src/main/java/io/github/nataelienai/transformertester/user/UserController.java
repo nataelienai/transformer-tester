2120d739fffcfb255413cbc74cd22f128227a1ed
Natã Elienai <nata.elienai@gmail.com>
2022-11-26 18:57:05 -0300

    feat: add endpoint to delete user by id

--- src/main/java/io/github/nataelienai/transformertester/user/UserController.java
+++ src/main/java/io/github/nataelienai/transformertester/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,6 +53,12 @@ public class UserController {
       @Valid @RequestBody UserInputDto userInputDto
   ) {
     return userService.update(id, userInputDto);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteById(@PathVariable("id") String id) {
+    userService.deleteById(id);
   }
 
   @ExceptionHandler(EmailAlreadyUsedException.class)
