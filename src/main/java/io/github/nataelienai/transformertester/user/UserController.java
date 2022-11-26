235f610b62f2dae846ffc5aac7021532b0183493
Natã Elienai <nata.elienai@gmail.com>
2022-11-26 14:07:46 -0300

    feat: add UserController with endpoint to create users

---
+++ src/main/java/io/github/nataelienai/transformertester/user/UserController.java
@@ -0,0 +1,28 @@
+package io.github.nataelienai.transformertester.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  private final UserService userService;
+
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public User create(@RequestBody UserInputDto userInputDto) {
+    return userService.create(userInputDto);
+  }
+
+}
