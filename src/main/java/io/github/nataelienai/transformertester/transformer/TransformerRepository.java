5fa763b098e3a09db1b5f324ac251a7b4550dc4c
Natã Elienai <nata.elienai@gmail.com>
2022-11-27 13:18:56 -0300

    fix: add query annotation to findAllById method from TransformerRepository interface

--- src/main/java/io/github/nataelienai/transformertester/transformer/TransformerRepository.java
+++ src/main/java/io/github/nataelienai/transformertester/transformer/TransformerRepository.java
@@ -2,9 +2,13 @@ package io.github.nataelienai.transformertester.transformer;
 
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransformerRepository extends MongoRepository<Transformer, String> {
+
+  @Query("{ '_id': { $in: ?0 } }")
   List<Transformer> findAllById(List<String> ids);
+
 }
