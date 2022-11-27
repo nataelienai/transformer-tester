831fef56e55029d644d4ebe6bb482f798ae9d4fc
Natã Elienai <nata.elienai@gmail.com>
2022-11-27 11:20:40 -0300

    feat: add field validation annotations to UpdateTransformerDto

--- src/main/java/io/github/nataelienai/transformertester/transformer/UpdateTransformerDto.java
+++ src/main/java/io/github/nataelienai/transformertester/transformer/UpdateTransformerDto.java
@@ -1,5 +1,7 @@
 package io.github.nataelienai.transformertester.transformer;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +11,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UpdateTransformerDto {
 
+  @NotBlank(message = "Field 'name' must not be blank")
   private String name;
+
+  @NotNull(message = "Field 'internalNumber' must not be null")
   private Double internalNumber;
+
+  @NotNull(message = "Field 'tensionClass' must not be null")
   private Double tensionClass;
+
+  @NotNull(message = "Field 'potency' must not be null")
   private Double potency;
+
+  @NotNull(message = "Field 'current' must not be null")
   private Double current;
 
 }
