ecb65331f18a846b7cfa488b14474a73cb9164ac
Natã Elienai <nata.elienai@gmail.com>
2022-11-26 21:49:23 -0300

    feat: add Transformer entity

---
+++ src/main/java/io/github/nataelienai/transformertester/transformer/Transformer.java
@@ -0,0 +1,57 @@
+package io.github.nataelienai.transformertester.transformer;
+
+import java.time.Instant;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.data.mongodb.core.mapping.Field;
+import io.github.nataelienai.transformertester.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document("transformers")
+public class Transformer {
+
+  @Id
+  private String id;
+
+  private String name;
+
+  @Field("internal_number")
+  private double internalNumber;
+
+  @Field("tension_class")
+  private double tensionClass;
+
+  private double potency;
+
+  private double current;
+
+  @CreatedDate
+  @Field("created_at")
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Field("updated_at")
+  private Instant updatedAt;
+
+  @DocumentReference
+  private User user;
+
+  public Transformer(String name, double internalNumber, double tensionClass,
+      double potency, double current, User user) {
+    this.name = name;
+    this.internalNumber = internalNumber;
+    this.tensionClass = tensionClass;
+    this.potency = potency;
+    this.current = current;
+    this.user = user;
+  }
+
+}
