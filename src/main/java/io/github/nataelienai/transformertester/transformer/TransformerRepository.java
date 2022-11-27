d26196f0541b6af8cfa71277ef6e3be336508a7e
Natã Elienai <nata.elienai@gmail.com>
2022-11-27 12:42:30 -0300

    feat: add findAllById method to TransformerRepository interface

--- src/main/java/io/github/nataelienai/transformertester/transformer/TransformerRepository.java
+++ src/main/java/io/github/nataelienai/transformertester/transformer/TransformerRepository.java
@@ -1,8 +1,10 @@
 package io.github.nataelienai.transformertester.transformer;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransformerRepository extends MongoRepository<Transformer, String> {
+  List<Transformer> findAllById(List<String> ids);
 }
