c94ad59d33e0ed80611348f34d5d9453d62f2d95
Natã Elienai <nata.elienai@gmail.com>
2022-11-27 12:47:25 -0300

    feat: add endpoint to create test to TestController

---
+++ src/main/java/io/github/nataelienai/transformertester/test/TestController.java
@@ -0,0 +1,28 @@
+package io.github.nataelienai.transformertester.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tests")
+public class TestController {
+
+  private final TestService testService;
+
+  @Autowired
+  public TestController(TestService testService) {
+    this.testService = testService;
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Test create(@RequestBody CreateTestDto createTestDto) {
+    return testService.create(createTestDto);
+  }
+
+}
