b7a5c9cd7d3cbc91f620bd2dad95c83496a724a0
Natã Elienai <nata.elienai@gmail.com>
2022-11-27 11:34:39 -0300

    feat: add endpoint to delete transformer by id to TransformerController

--- src/main/java/io/github/nataelienai/transformertester/transformer/TransformerController.java
+++ src/main/java/io/github/nataelienai/transformertester/transformer/TransformerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,12 @@ public class TransformerController {
       @Valid @RequestBody UpdateTransformerDto updateTransformerDto
   ) {
     return transformerService.updateById(id, updateTransformerDto);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteById(@PathVariable("id") String id) {
+    transformerService.deleteById(id);
   }
 
 }
