9b1cfde14c0e42d05961530aae57f766c5752bd6
Natã Elienai <nata.elienai@gmail.com>
2022-11-26 23:53:06 -0300

    feat: add validation exception handler method to TransformerController

--- src/main/java/io/github/nataelienai/transformertester/transformer/TransformerController.java
+++ src/main/java/io/github/nataelienai/transformertester/transformer/TransformerController.java
@@ -3,11 +3,15 @@ package io.github.nataelienai.transformertester.transformer;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.nataelienai.transformertester.user.ErrorResponse;
 
 @RestController
 @RequestMapping("/transformers")
@@ -26,6 +30,17 @@ public class TransformerController {
       @Valid @RequestBody CreateTransformerInputDto createTransformerInputDto
   ) {
     return transformerService.create(createTransformerInputDto);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    FieldError fieldError = exception.getFieldError();
+    if (fieldError == null) {
+      return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Validation failed");
+    }
+    String message = fieldError.getDefaultMessage();
+    return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
   }
 
 }
