0bf6b3969740696a8c2add0df6b098ab8a98a306
Natã Elienai <nata.elienai@gmail.com>
2022-11-27 00:17:09 -0300

    refactor: move ErrorResponse to global package

--- src/main/java/io/github/nataelienai/transformertester/transformer/TransformerController.java
+++ src/main/java/io/github/nataelienai/transformertester/transformer/TransformerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import io.github.nataelienai.transformertester.user.ErrorResponse;
+import io.github.nataelienai.transformertester.global.ErrorResponse;
 import io.github.nataelienai.transformertester.user.UserNotFoundException;
 
 @RestController
