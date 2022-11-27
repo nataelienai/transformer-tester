f33c406cfcc3d0bc64cd79936355c28d2799fef0
Natã Elienai <nata.elienai@gmail.com>
2022-11-27 17:19:02 -0300

    feat: add UpdateReportDto class

---
+++ src/main/java/io/github/nataelienai/transformertester/report/UpdateReportDto.java
@@ -0,0 +1,15 @@
+package io.github.nataelienai.transformertester.report;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateReportDto {
+
+  private String name;
+  private String status;
+
+}
